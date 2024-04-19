96e556a6358445f301c9b8b8624b3790548d2f84
Marti Serra <martiserramolina99@gmail.com>
2024-04-19 11:07:18 +0200

    Moved stories file

--- src/Test.stories.tsx
+++ src/Test.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import Test from "./Test";
+import Test from "./v2/Test";
 
 const meta: Meta<typeof Test> = {
   title: "Components/Quick Menu",
